34b0bb5edcdd4130c3572380c5c1ea2859eed578
nahu <nahubarrera321@gmail.com>
2024-01-18 19:06:37 -0300

    corrección de errores

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ app.use(cors());
 const bcrypt = require("bcryptjs");
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
-
+require('dotenv').config();
 const jwt = require("jsonwebtoken");
+
 var nodemailer = require("nodemailer");
 
-const JWT_SECRET =
-  "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jbkj?[]]pou89ywe";
-
-const mongoUrl =
-  "mongodb+srv://Nxhuu:j94fJYvechnI9tHP@cluster0.tvcyj23.mongodb.net/";
+const JWT_SECRET = process.env.JWT_SECRET;
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose
   .connect(mongoUrl, {
